98782f36a070d44c00bf87100d89c7923b288667
Arbab Naved <81221340+Hajinaved@users.noreply.github.com>
2024-09-21 17:08:53 +0530

    Add files via upload

---
+++ Generic.bat
@@ -0,0 +1,6 @@
+:: you can make your custom anime shorcut by just making small adjustment in here
+:: place the name of your anime in place of some-anime-name , and save this fill
+::when you will run this bat it will get you to that anime directly
+
+@echo off
+"C:\Program Files\Git\bin\bash.exe" --login -i -c "ani-cli 'some-anime-name' --vlc"
