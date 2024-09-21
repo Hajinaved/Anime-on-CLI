1fdd8348fddb1d17492918405399700ec8c14597
Arbab Naved <81221340+Hajinaved@users.noreply.github.com>
2024-09-22 00:01:20 +0530

    Add files via upload

--- DemonSlayer.bat
+++ DemonSlayer.bat
@@ -2,4 +2,4 @@
 :: place the name of your anime in place of some-anime-name , and save this fill
 ::when you will run this bat it will get you to that anime directly
 
-"C:\Program Files\Git\bin\bash.exe" --login -i -c "ani-cli 'classroom of elites' --vlc"
+"C:\Program Files\Git\bin\bash.exe" --login -i -c "ani-cli 'demon slayer' --vlc"
--- RecordOfRagnarok.bat
+++ RecordOfRagnarok.bat
@@ -2,4 +2,5 @@
 :: place the name of your anime in place of some-anime-name , and save this fill
 ::when you will run this bat it will get you to that anime directly
 
-"C:\Program Files\Git\bin\bash.exe" --login -i -c "ani-cli 'classroom of elites' --vlc"
+@echo off
+"C:\Program Files\Git\bin\bash.exe" --login -i -c "ani-cli 'record of ragnarok' --vlc"
